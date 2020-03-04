1a8f62c41cd6b1f62303fa0e2ef800f975f0aca5
Jiajia Chen <chenjengjia@gmail.com>
2020-03-03 17:09:22 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+new Vue({
+	el: '#llama',
+	data: {
+		header: 'Llama Facts',
+		items:[
+		'Llamas make excellent guards for herds of small animals.',
+		'They are very social and will ‘adopt’ a group of sheep or goats as their own herd.',
+		'Then they will protect the herd by chasing off coyotes and other predators.',
+		'Llamas are smart. They can distinguish between the neighbor’s dog and a predatory coyote.',
+		'Llamas are the camel’s hippie cousins. ',
+		'They belong to a group of animals called camelids that also includes alpacas.',
+		'All camelids spit or stick out their tongue when they are annoyed.',
+'One of the ways llamas communicate is by humming.',
+'Llamas are diabetic — sort of. The OSU College of Veterinary Medicine (CVM) is using their herd of 30 llamas and alpacas to study how certain hormones affect blood sugar.',
+'Because the metabolism of llamas is very similar to that of a human diabetic, the results of this research may provide insight into human diabetes treatment.',
+	     
+		],
+		noLlamaFacts: false
+	}
+});
